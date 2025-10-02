61221b4796ef8103692e50e0d66debc1a06d4d42
Abichal Shukla <abichalshukla9963@gmail.com>
2025-10-01 20:36:11 -0700

    Update Api.jsx
    add tha original url

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://shop-management-vert.vercel.app/api",
+  baseURL: "https://shop-management-pwpn.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
